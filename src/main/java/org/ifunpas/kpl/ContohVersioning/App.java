4506d45075b03bfa55352a838401707ea940c198
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2018-12-15 06:06:22 +0700

    private String id

--- src/main/java/org/ifunpas/kpl/ContohVersioning/App.java
+++ src/main/java/org/ifunpas/kpl/ContohVersioning/App.java
@@ -4,11 +4,11 @@ package org.ifunpas.kpl.ContohVersioning;
  * Hello world!
  *
  */
-public class App 
-{
-	String id;
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private String id;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
